aff673797ef394121ae68d6bca336b9ed123b84d
vassvik <mvassvik@gmail.com>
2017-09-03 16:29:55 +0200

    Add next example

---
+++ main1.c
@@ -0,0 +1,66 @@
+#include "helpers.h" // declares window, resx and resy
+
+int main() {
+	int resx = 640, resy = 480;
+	GLFWwindow *window = init(resx, resy); // initializes glfw, creates a window and loads opengl function pointers (via glad)
+	if (window == NULL) {
+		glfwTerminate();
+		return -1;
+	}
+	
+	GLuint program = load_shaders("vertex_shader1.vs", "fragment_shader.fs");
+    if (program == 0) {
+        fprintf(stderr, "Could not load shaders. Exiting\n");
+        glfwTerminate();
+        return -2;
+    }
+    
+        // setup vao
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    // setup vbo
+    float vertex_data[] = {
+        -0.3, -0.3,
+         0.3, -0.3,
+         0.0,  0.5,
+    };
+
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_data), &vertex_data[0], GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, NULL);
+    
+    
+    
+    glClearColor(0.2, 0.3, 0.4, 1.0);
+    while (!glfwWindowShouldClose(window) && glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_RELEASE) {
+        calculate_frame_timings(window);
+
+        glfwPollEvents();
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(program);
+
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glfwSwapBuffers(window);
+    }
+
+    glDeleteProgram(program);
+    glDeleteBuffers(1, &vbo);
+    glDeleteVertexArrays(1, &vao);
+    glfwTerminate();
+    return 0;
+}
+
+
+
+
